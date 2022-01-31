7fc270415912f80347f33ad6b60e008ce285aeb9
Piousangel <yyh7654@gmail.com>
2022-01-31 17:15:38 +0900

    1/31

--- FlaskProject1/FindUpdate.py
+++ FlaskProject1/FindUpdate.py
@@ -8,6 +8,12 @@ score = db.movies.find_one({'title' : '매트릭스'})['star']
 
 all_movies = list(db.movies.find({'star' : score}))
 
-# 영화 매트릭스와 평점이 같은 영화리스트 db에서 찾기
-for movie in all_movies:
-    print(movie['title'])
+# # 영화 매트릭스와 평점이 같은 영화리스트 db에서 찾기
+# for movie in all_movies:
+#     print(movie['title'])
+
+# 평점 0으로 수정
+db.movies.update_one({'title' : '매트릭스'}, {'$set' : {'star' : '0'}})
+user = db.movies.find_one({'title' : '매트릭스'})
+print(user)
+
--- FlaskProject2/app.py
+++ FlaskProject2/app.py
@@ -1,11 +1,11 @@
 # 통상적으로 flask 서버를 돌리는 파일은 app.py라고 이름 짓습니다.
 
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')   #서버 실행시 기본페이지
 def home():
-    return 'This is Home!'
+    return render_template('index.html')
 
 @app.route('/mypage')
 def mypage():
