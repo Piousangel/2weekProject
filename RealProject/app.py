69c0cc7a8d3482e31e284b91f6b3976d70488f0f
Piousangel <yyh7654@gmail.com>
2022-02-01 01:31:02 +0900

    2/1

--- RealProject/app.py
+++ RealProject/app.py
@@ -18,18 +18,16 @@ def listing():
     return jsonify({'result' : 'success', 'msg' : 'GET 연결되었습니다'})
 
 
-## API 역할을 하는 영역
 @app.route('/memo', methods=['POST'])
 def post_article():
-    # 1.클라이언트로부터 데이터를 받기
-    url_receive = request.form['url_give'] #클라이언트로부터 url을 받는 부분
-    comment_receive = request.form['comment_give'] #클라이언트로부터 comment를 받는 부분
+    # 1. 클라이언트로부터 데이터를 받기
+    url_receive = request.form['url_give']  # 클라이언트로부터 url을 받는 부분
+    comment_receive = request.form['comment_give']  # 클라이언트로부터 comment를 받는 부분
 
     # 2. meta tag를 스크래핑하기
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'
-    }
-    data = requests.get(url_receive, headers = headers)
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+    data = requests.get(url_receive, headers=headers)
     soup = BeautifulSoup(data.text, 'html.parser')
 
     og_image = soup.select_one('meta[property="og:image"]')
@@ -40,13 +38,15 @@ def post_article():
     url_description = og_description['content']
     url_image = og_image['content']
 
-    article = {'url' : url_receive, 'title' : url_title, 'desc' : url_description, 'image' : url_image, 'comment' : comment_receive}
+    article = {'url': url_receive, 'title': url_title, 'desc': url_description, 'image': url_image,
+               'comment': comment_receive}
 
-    # 3.mongoDB에 데이터를 넣기
+    # 3. mongoDB에 데이터를 넣기
     db.articles.insert_one(article)
 
-    return jsonify({'result' : 'success', 'msg' : 'POST 연결되었습니다'})
+    return jsonify({'result': 'success'})
 
+    
 if __name__ == '__main__':
    app.run('0.0.0.0',port=5000,debug=True)
 
