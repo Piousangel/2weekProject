a3e8f82d5825e6a4a3876ba7e7f597f1c833bc75
Piousangel <yyh7654@gmail.com>
2022-02-01 15:10:19 +0900

    2/1

--- RealProject/app.py
+++ RealProject/app.py
@@ -1,21 +1,17 @@
 from flask import Flask, render_template, jsonify, request
-app = Flask(__name__)
-
 import requests
 from bs4 import BeautifulSoup
-from pymongo import MongoClient
-client = MongoClient('localhost', 27017)
-db = client.dbjungle
+from pymongo import MongoClient  # pymongo를 임포트 하기(패키지 인스톨 먼저 해야겠죠?)
+
+app = Flask(__name__)
+
+client = MongoClient('localhost', 27017)  # mongoDB는 27017 포트로 돌아갑니다.
+db = client.dbsparta  # 'dbsparta'라는 이름의 db를 만들거나 사용합니다.
+
 
 @app.route('/')
 def home():
     return render_template('index.html')
-
-@app.route('/memo', methods=['GET'])
-def listing():
-    # 1. 모든 document 찾기 & _id 값은 출력에서 제외하기
-    # 2. articles라는 키 값으로 영화정보 내려주기
-    return jsonify({'result' : 'success', 'msg' : 'GET 연결되었습니다'})
 
 
 @app.route('/memo', methods=['POST'])
@@ -46,7 +42,14 @@ def post_article():
 
     return jsonify({'result': 'success'})
 
-    
-if __name__ == '__main__':
-   app.run('0.0.0.0',port=5000,debug=True)
 
+@app.route('/memo', methods=['GET'])
+def read_articles():
+    # 1. mongoDB에서 _id 값을 제외한 모든 데이터 조회해오기 (Read)
+    result = list(db.articles.find({}, {'_id': 0}))
+    # 2. articles라는 키 값으로 article 정보 보내주기
+    return jsonify({'result': 'success', 'articles': result})
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
