5c10be47374bb70f527c8404fda720c5c4c619b3
Piousangel <yyh7654@gmail.com>
2022-02-03 03:42:37 +0900

    2/3

--- MoviestarProject/app.py
+++ MoviestarProject/app.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template, jsonify, request
 
 app = Flask(__name__)
 
-client = MongoClient('localhost', 27017)
+client = MongoClient('localhost', 27017)      #나중에 'mongodb://test:test@아이피'
 db = client.dbjungle
 
 
@@ -17,36 +17,36 @@ def home():
 # API 역할을 하는 부분
 @app.route('/api/list', methods=['GET'])
 def show_stars():
-    # 1. db에서 mystar 목록 전체를 검색합니다. ID는 제외하고 like 가 많은 순으로 정렬합니다.
-    # 참고) find({},{'_id':False}), sort()를 활용하면 굿!
+    # 1. db에서 mystar 목록 전체를 검색 ID는 제외하고 like 가 많은 순으로 정렬
+    # 참고) find({},{'_id':False}), sort() 사용
     stars = list(db.mystar.find({}, {'_id': False}).sort('like', -1))
-    # 2. 성공하면 success 메시지와 함께 stars_list 목록을 클라이언트에 전달합니다.
+    # 2. 성공하면 success 메시지와 함께 stars_list 목록을 클라이언트에 전달
     return jsonify({'result': 'success', 'stars_list': stars})
 
 
 @app.route('/api/like', methods=['POST'])
 def like_star():
-    # 1. 클라이언트가 전달한 name_give를 name_receive 변수에 넣습니다.
+    # 1. 클라이언트가 전달한 name_give를 변수에 저장
     name_receive = request.form['name_give']
-    # 2. mystar 목록에서 find_one으로 name이 name_receive와 일치하는 star를 찾습니다.
+    # 2. mystar 목록에서 find_one 사용
     star = db.mystar.find_one({'name': name_receive})
-    # 3. star의 like 에 1을 더해준 new_like 변수를 만듭니다.
+
     new_like = star['like'] + 1
 
-    # 4. mystar 목록에서 name이 name_receive인 문서의 like 를 new_like로 변경합니다.
-    # 참고: '$set' 활용하기!
+    # 3. mystar 목록에서 name이 name_receive인 문서의 like 업뎃  '$set'사용
+    
     db.mystar.update_one({'name': name_receive}, {'$set': {'like': new_like}})
-    # 5. 성공하면 success 메시지를 반환합니다.
+    # success 메시지를 반환합니다.
     return jsonify({'result': 'success'})
 
 
 @app.route('/api/delete', methods=['POST'])
 def delete_star():
-    # 1. 클라이언트가 전달한 name_give를 name_receive 변수에 넣습니다.
+
     name_receive = request.form['name_give']
-    # 2. mystar 목록에서 delete_one으로 name이 name_receive와 일치하는 star를 제거합니다.
+    # name이 name_receive와 일치하는 star를 제거합니다.
     db.mystar.delete_one({'name': name_receive})
-    # 3. 성공하면 success 메시지를 반환합니다.
+    #success 메시지 리턴
     return jsonify({'result': 'success'})
 
 if __name__ == '__main__':
--- MoviestarProject/init_db.py
+++ MoviestarProject/init_db.py
@@ -3,20 +3,19 @@ from bs4 import BeautifulSoup
 
 from pymongo import MongoClient
 
-client = MongoClient('localhost', 27017)
+client = MongoClient('localhost', 27017) #나중에 'mongodb://test:test@아이피'
 db = client.dbjungle
 
-##### 사용할 데이터를 웹 스크래핑, 데이터베이스에 저장 #####
 
-# DB에 저장할 영화인들의 출처 url 가져오기
-
+# DB에 저장할 영화인들의 출처 url을 가져오기
 def get_urls():
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'
-    }
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
     data = requests.get('https://movie.naver.com/movie/sdb/rank/rpeople.nhn', headers=headers)
+
     soup = BeautifulSoup(data.text, 'html.parser')
-    trs = soup.select('#old_content > table > tdoby > tr')
+
+    trs = soup.select('#old_content > table > tbody > tr')
 
     urls = []
     for tr in trs:
@@ -28,12 +27,13 @@ def get_urls():
 
     return urls
 
-# 출처 url로 부터 영화인들의 사진, 이름, 최근작 정보를 가져오고 mystar 컬렉션에 저장
+
+# 출처 url로부터 영화인들의 사진, 이름, 최근작 정보를 가져오고 mystar 콜렉션에 저장
 def insert_star(url):
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'
-    }
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
     data = requests.get(url, headers=headers)
+
     soup = BeautifulSoup(data.text, 'html.parser')
 
     name = soup.select_one('#content > div.article > div.mv_info_area > div.mv_info.character > h3 > a').text
@@ -42,23 +42,24 @@ def insert_star(url):
         '#content > div.article > div.mv_info_area > div.mv_info.character > dl > dd > a:nth-child(1)').text
 
     doc = {
-        'name' : name,
-        'img_url' : img_url,
-        'recent' : recent_work,
-        'url' : url,
-        'like' : 0
+        'name': name,
+        'img_url': img_url,
+        'recent': recent_work,
+        'url': url,
+        'like': 0
     }
 
     db.mystar.insert_one(doc)
-    print('저장완료', name)
+    print('완료!', name)
 
-    # 기존 mystar 콜렉션을 삭제하고, 출처 url들을 가져온 후, 크롤링하여 DB에 저장
-    def insert_all():
-        db.mystar.drop()  # mystar 콜렉션을 모두 삭제
-        urls = get_urls()
 
-        for url in urls:
-            insert_star(url)
+# 기존 mystar 콜렉션을 삭제하고, 출처 url들을 가져온 후, 크롤링하여 DB에 저장
+def insert_all():
+    db.mystar.drop()  # mystar 콜렉션을 모두 지우기
+    urls = get_urls()
+    for url in urls:
+        insert_star(url)
 
-    ## 실행
-    insert_all()
+
+### 실행하기
+insert_all()
